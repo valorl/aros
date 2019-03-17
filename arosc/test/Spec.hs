3339ab114337f6aba1322c55f1e4fdfbbcf7e6d6
valorl <valer.orlovsky@gmail.com>
2019-03-17 22:47:05 +0100

    :white_check_mark: Add a basic .aros code test + test examples
    Using the HSpec library here (https://hspec.github.io), also used in Chapter 14 in the Haskell book

--- arosc/test/Spec.hs
+++ arosc/test/Spec.hs
@@ -1,2 +1,33 @@
+import Test.Hspec
+import Syntax
+import qualified Parser
+import qualified Lexer
+
+
+resDir = "test/resources/" :: FilePath
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  -- EXAMPLE
+  describe "Addition (Example)" $ do
+    let onePlusOne = 1 + 1
+
+    it "1 + 1 should be greater than 1" $ do
+      (onePlusOne > 1) `shouldBe` True
+    it "1 + 1 should equal 2" $ do
+      onePlusOne `shouldBe` 2
+
+
+  describe "Parsing" $ do
+    it "Basic shape declaration in grid def should parse correctly (01-simple-decl-and-grid)" $ do
+      code <- readFile (resDir ++ "01-simple-decl-and-grid.aros")
+      let ast = (Parser.parseAros . Lexer.lexAros) code
+      let correctAst = Program
+                        [ SDecl
+                        "myShape" (UShape [ SMPoint (VVec (Vector (ILit 1) (ILit 1))) ])
+                        ]
+                        (GridDef
+                        (SShape
+                                (SVector (ILit 5) (ILit 5))
+                                [ SMIdent "myShape" (VVec (Vector (ILit 1) (ILit 2))) ]))
+      ast `shouldBe` correctAst
