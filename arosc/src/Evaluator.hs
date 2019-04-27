b43b23d01005520fd35792c7139ecb3471892888
Felix Gravila <felixgravila@gmail.com>
2019-04-27 19:55:02 +0200

    :hankey: Commit handleExp ListExp problem for help

--- arosc/src/Evaluator.hs
+++ arosc/src/Evaluator.hs
@@ -1,41 +1,42 @@
 module Evaluator where
 import Parser
 import Syntax
-import Data.Map
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Set (Set)
+import qualified Data.Set as Set
 
-data TypedVariable = TInt Int
-                   | TVec (Int, Int)
-                   | TBool Bool
-                   | TLInt [Int]
-                   | TLVec [(Int, Int)]
-                   | TSInt [Int]
-                   | TSVec [(Int, Int)]
-                   | TLambda
-                   deriving Show
+
+data Value = TInt Int
+           | TVec (Int, Int)
+           | TBool Bool
+           | TList [Value]
+           | TLambda
+           deriving Show
 
 
 parsed :: Either String Program
 parsed = Parser.parseAros "" "int myint = 5 ; grid < 2 , 4 > , { <1,1> } routeRobot [ <1,1> ] "
 
 evalTree :: Either String Program -> Maybe String
-evalTree (Right x) = evaluateProgram x Data.Map.empty
+evalTree (Right x) = evaluateProgram x Map.empty
 evalTree (Left _) = Nothing
 
 -- Parses definitions into a map, then calls handleRobot
-evaluateProgram :: Program -> Map String TypedVariable -> Maybe String
+evaluateProgram :: Program -> Map String Value -> Maybe String
 evaluateProgram (Program ((Decl dtype ident expr):xs) grd wpts ) defs =
   case (computeDecl dtype defs expr) of
     Nothing -> Nothing
-    Just computedDecl -> evaluateProgram (Program xs grd wpts) (insert ident computedDecl defs)
+    Just computedDecl -> evaluateProgram (Program xs grd wpts) (Map.insert ident computedDecl defs)
 evaluateProgram (Program [] grd wpts) defs = handleRobot (handleGrid grd defs) wpts defs
 
 
-computeDecl :: DeclType -> Map String TypedVariable -> Exp -> Maybe TypedVariable
+computeDecl :: DeclType -> Map String Value -> Exp -> Maybe Value
 computeDecl dtype defs expr = handleExp defs expr
 
 -- TODO
-handleGrid :: GridDef -> Map String TypedVariable -> Maybe TypedVariable
-handleGrid _ _ = Just (TSVec [ (1, 1), (2, 2), (3, 3) ])
+handleGrid :: GridDef -> Map String Value -> Maybe Value
+handleGrid _ _ = Just (TList [TVec (1, 1), TVec (2, 2), TVec (3, 3) ])
 
 vvv :: Exp
 vvv = VectorExp (IntegerExp 69) (BooleanExp True)
@@ -44,8 +45,8 @@ vvv2 = VectorExp (IntegerExp 69) (IntegerExp 420)
 vvv3 :: Exp
 vvv3 = ListExp [(IntegerExp 1), (IntegerExp 2), (IntegerExp 3)]
 
-handleExp :: Map String TypedVariable -> Exp -> Maybe TypedVariable
-handleExp defs (VariableExp ident) = Data.Map.lookup ident defs
+handleExp :: Map String Value -> Exp -> Maybe Value
+handleExp defs (VariableExp ident) = Map.lookup ident defs
 handleExp defs (ParenExp expr) = handleExp defs expr
 handleExp _ (IntegerExp i) = Just $ TInt i
 handleExp _ (BooleanExp b) = Just $ TBool b
@@ -58,15 +59,11 @@ handleExp defs (VectorExp a b) =
     (TInt ub) <- cb
     return (TVec (ua, ub))
 
-handleExp defs (ListExp (x:xs)) =
-  let
-    handled = handleExp defs x
-  in do
-    (TInt ua) <- handled
-    (TLInt ub) <- handleExp defs (ListExp xs)
-    return (TLInt (ua : ub))
+handleExp defs (ListExp expList) = do
+  mapped <- map (handleExp defs) expList
+  return $ TList mapped
 
-handleExp _ (ListExp []) = Just $ TLInt []
+handleExp _ (ListExp []) = Just $ TList []
 
 
 handleExp _ _ = Nothing
@@ -74,6 +71,6 @@ handleExp _ _ = Nothing
 
 
 -- TODO
-handleRobot :: Maybe TypedVariable -> [Exp] -> Map String TypedVariable -> Maybe String
-handleRobot (Just (TSVec (x:xs))) wpts defs = Just (show defs)
+handleRobot :: Maybe Value -> [Exp] -> Map String Value -> Maybe String
+handleRobot (Just (TList (x:xs))) wpts defs = Just (show defs)
 handleRobot _ _ _ = Nothing
